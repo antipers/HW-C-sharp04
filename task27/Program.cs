d094c5634d09b1cf328e51b3fbb8097ceae78bfd
antipers <antipers@gmail.com>
2022-07-08 20:36:48 +0300

    completed

--- task27/Program.cs
+++ task27/Program.cs
@@ -1,4 +1,5 @@
-﻿System.Console.WriteLine("Введите число- ");
+﻿/* пробовал через получение кол-ва цифр в числе и создания массива. НЕУДАЧНО
+System.Console.WriteLine("Введите число- ");
 int number = Convert.ToInt32(Console.ReadLine());
 
 int digit = 0;
@@ -19,9 +20,27 @@ else if (number < 0)
 }
 System.Console.WriteLine(digit);
 
-int[] array = new int[digit];
-for (int i = digit; i <= 0; i--)
+int size = Convert.ToInt32(Console.ReadLine())
+int[] array = new int[size];
+for (int i = array.Length; i >= 0; i--)
 {
-    array[i] = (number %= 10) / 10;
-    System.Console.WriteLine($"{array[0]} ,");
+    array[i] = number % 10;
+    number/= 10;
+
 }
+foreach (int i in array)
+    Console.WriteLine(i); */
+
+System.Console.WriteLine("Введите число- ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void sum(int value)
+{
+    int result = 0;
+    for (; value > 0; value /= 10)
+    {
+        result += value % 10;
+    }
+    System.Console.WriteLine(result);
+}
+sum(number);
